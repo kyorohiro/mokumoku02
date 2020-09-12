a7c96de39e13140d90949caed1e20ee4ab7ebffc
kyorohiro <kyorohiro@gmail.com>
2020-09-12 19:01:48 +0900

    extract menu page

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './client.dart' as client;
-
+import './menupage.dart' as menupage;
 
 class CartItem {
   String id;
@@ -46,15 +46,15 @@ class Cart {
 
 //
 // golobal prop
-List<client.Item> __todoMenus;
-Cart __cart = new Cart();
+List<client.Item> todoMenus;
+Cart cart = new Cart();
 
 void main() async {
   // test 
-  __todoMenus = await client.createMenuClient().getMenus();
+  todoMenus = await client.createMenuClient().getMenus();
 
   runApp( MaterialApp(
-    home: MyApp()
+    home: menupage.MyMenuPage()
   ));
 }
 
@@ -82,8 +82,8 @@ class MyBasket extends StatelessWidget {
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
     var density = MediaQuery.of(context).devicePixelRatio; 
-    var itemIds =  __cart.items.keys.toList();
-    var items = itemIds.map((e) => menuItemWidget(screenWidth,__cart.items[e].item)).toList();
+    var itemIds =  cart.items.keys.toList();
+    var items = itemIds.map((e) => menuItemWidget(screenWidth,cart.items[e].item)).toList();
    // var items = itemIds.map((e) => Text("${__cart.items[e].name}")).toList();
 
     return Scaffold(
@@ -109,12 +109,12 @@ class MyBasket extends StatelessWidget {
   }
 
   Widget itemList() {
-    var items =  __cart.items.keys.toList();
+    var items =  cart.items.keys.toList();
     return 
     ListView.builder(
       itemCount: items.length,
       itemBuilder: (context, index) {
-      return Text("${__cart.items[items[index]].name}");
+      return Text("${cart.items[items[index]].name}");
     },);
     //Text("Bucket !!");
   }
@@ -157,159 +157,7 @@ Widget menuItemWidget(double screenWidth, client.Item item){
         ));
 }
 
-//
-// Start and Menus Page
-//
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Menu"),
-        actions: [
-          RaisedButton(
-            child: Icon(
-              Icons.shopping_basket
-            ),
-            onPressed: (){
-              print("click backet");
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return MyBasket();
-              },));
-          })
-        ],
-      ),
-      body: MenuItemWidget(),
-    );
-  }
-}
 
-
-
-class MenuItemWidget extends StatefulWidget {
-  @override
-  _MenuItemWidgetState createState() => _MenuItemWidgetState();
-}
-
-class _MenuItemWidgetState extends State<MenuItemWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return testWidget();
-  }
-
-  Widget testWidget() {
-
-    return ListView.builder(
-      itemCount: __todoMenus.length,
-      itemBuilder: (context, index) {
-        var item = __todoMenus[index];
-        var title = item.title; 
-        var id = item.id;
-        
-        return Container(
-          height: 200,
-          margin: EdgeInsets.all(10),
-          padding: EdgeInsets.all(10),
-          color: (index%2==0?Colors.red:Colors.blue),
-          child: Row(
-
-            mainAxisSize: MainAxisSize.min ,
-            children: [
-              //
-              // Container(width: 200,height: 200, color: Colors.amber,),
-              ImageWithLoading(200,item.getImage),
-              //
-              //
-              Expanded(child: 
-              //Container(height: 200,color: Colors.white30, child: Text("xx"),),),            
-              Container(
-                color: Colors.white30,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 60,
-                      color: Colors.black38,
-                      child: Text("Food Name",style: TextStyle(fontSize: 40.0),),
-                    ),                  
-                    Container(
-                      height: 60,
-                      child: 
-                      Text("desciption. asdfasdf.asdfa.sf..asdfasf,as ,asfd ,asf, asf,d a,sd,f "),
-                    ),
-                    Container(
-                      width: 3000,
-                      height: 60,
-                      color: Colors.black38,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          // + button
-                          Container(
-                            width: 45,
-                            child: 
-                              RaisedButton(
-                                onPressed: (){
-                                  print("clicked ${id}");
-                                  __cart.plusItem(
-                                    CartItem()
-                                    ..id = item.id
-                                    ..name = item.title
-                                    ..price = item.price
-                                    ..item = item
-                                    );
-                                    setState(() {
-                                      
-                                    });
-                                },
-                                child: Text("+"),
-                              ),
-                              
-                          ),
-                          
-                          Container(
-                            color: Colors.white,
-                            height: 35,
-                            width: 45,
-                            child: Align(alignment: Alignment.center,child:Text("${__cart.getNumberOfOrder(item.id)}")),
-                          ),
-                          // - button
-                          Container(
-                            width: 45,
-                            child: 
-                              RaisedButton(
-                                onPressed: (){
-                                  print("clicked ${id}");
-                                  __cart.minusItem(
-                                    CartItem()
-                                    ..id = item.id
-                                    ..name = item.title
-                                    ..price = item.price
-                                    ..item = item
-                                    );
-                                    setState(() {
-                                      
-                                    });
-                                },
-                                child: Text("-"),
-                              ),
-                              
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 30),
-                            child: Text("800",style: TextStyle(fontSize: 40.0),textAlign: TextAlign.end,)
-                          )
-                        ],)
-                      
-                    ),    
-                  ],)
-              )),
-            ],
-          ),
-        );     
-      },);
-  }
-}
 
 typedef ImageGetFunction = Future<Image> Function();
 class ImageWithLoading extends StatefulWidget {
---
+++ myapp/lib/menupage.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import './client.dart' as client;
+import 'main.dart' as basketpage;
+import 'main.dart' as appcontext;
+import 'main.dart' as parts;
+//
+// Start and Menus Page
+//
+class MyMenuPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Menu"),
+        actions: [
+          RaisedButton(
+            child: Icon(
+              Icons.shopping_basket
+            ),
+            onPressed: (){
+              print("click backet");
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return basketpage.MyBasket();
+              },));
+          })
+        ],
+      ),
+      body: MenuItemWidget(),
+    );
+  }
+}
+
+
+
+class MenuItemWidget extends StatefulWidget {
+  @override
+  _MenuItemWidgetState createState() => _MenuItemWidgetState();
+}
+
+class _MenuItemWidgetState extends State<MenuItemWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return testWidget();
+  }
+
+  Widget testWidget() {
+
+    return ListView.builder(
+      itemCount: appcontext.todoMenus.length,
+      itemBuilder: (context, index) {
+        var item = appcontext.todoMenus[index];
+        var title = item.title; 
+        var id = item.id;
+        
+        return Container(
+          height: 200,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          color: (index%2==0?Colors.red:Colors.blue),
+          child: Row(
+
+            mainAxisSize: MainAxisSize.min ,
+            children: [
+              //
+              // Container(width: 200,height: 200, color: Colors.amber,),
+              parts.ImageWithLoading(200,item.getImage),
+              //
+              //
+              Expanded(child: 
+              //Container(height: 200,color: Colors.white30, child: Text("xx"),),),            
+              Container(
+                color: Colors.white30,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 60,
+                      color: Colors.black38,
+                      child: Text("Food Name",style: TextStyle(fontSize: 40.0),),
+                    ),                  
+                    Container(
+                      height: 60,
+                      child: 
+                      Text("desciption. asdfasdf.asdfa.sf..asdfasf,as ,asfd ,asf, asf,d a,sd,f "),
+                    ),
+                    Container(
+                      width: 3000,
+                      height: 60,
+                      color: Colors.black38,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          // + button
+                          Container(
+                            width: 45,
+                            child: 
+                              RaisedButton(
+                                onPressed: (){
+                                  print("clicked ${id}");
+                                  appcontext.cart.plusItem(
+                                    appcontext.CartItem()
+                                    ..id = item.id
+                                    ..name = item.title
+                                    ..price = item.price
+                                    ..item = item
+                                    );
+                                    setState(() {
+                                      
+                                    });
+                                },
+                                child: Text("+"),
+                              ),
+                              
+                          ),
+                          
+                          Container(
+                            color: Colors.white,
+                            height: 35,
+                            width: 45,
+                            child: Align(alignment: Alignment.center,child:Text("${appcontext.cart.getNumberOfOrder(item.id)}")),
+                          ),
+                          // - button
+                          Container(
+                            width: 45,
+                            child: 
+                              RaisedButton(
+                                onPressed: (){
+                                  print("clicked ${id}");
+                                  appcontext.cart.minusItem(
+                                    appcontext.CartItem()
+                                    ..id = item.id
+                                    ..name = item.title
+                                    ..price = item.price
+                                    ..item = item
+                                    );
+                                    setState(() {
+                                      
+                                    });
+                                },
+                                child: Text("-"),
+                              ),
+                              
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 30),
+                            child: Text("800",style: TextStyle(fontSize: 40.0),textAlign: TextAlign.end,)
+                          )
+                        ],)
+                      
+                    ),    
+                  ],)
+              )),
+            ],
+          ),
+        );     
+      },);
+  }
+}
