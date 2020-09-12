f9b4cc490e32170fd9ba0c4155110c62da8901d9
kyorohiro <kyorohiro@gmail.com>
2020-09-12 19:31:56 +0900

    ..

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -3,12 +3,20 @@ import './client.dart' as client;
 import './menupage.dart' as menupage;
 import './parts.dart' as parts;
 import './app.dart' as appcontext;
-
+import './cartpage.dart' as cartpage;
 void main() async {
   // todo : to load data in Widget
   appcontext.todoMenus = await client.createMenuClient().getMenus();
 
   runApp( MaterialApp(
+    routes: {
+      "/menu": (BuildContext context){
+        return menupage.MyMenuPage();
+      },
+      "/cart":  (BuildContext context){
+        return cartpage.MyBasket();
+      },
+    },
     home: menupage.MyMenuPage()
   ));
 }
